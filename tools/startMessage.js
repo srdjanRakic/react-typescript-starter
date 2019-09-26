72049ecd6d35c7b1da642a1a20be91965567be90
srdjanRakic <srgjan.rakic@gmail.com>
2019-09-26 16:41:45 +0200

    Add start scriot

---
+++ tools/startMessage.js
@@ -0,0 +1,6 @@
+import { chalkSuccess } from './chalkConfig';
+
+console.log(chalkSuccess('============================'));
+console.log(chalkSuccess('    = YOUR PROJECT NAME =    '));
+console.log(chalkSuccess('============================'));
+console.log(chalkSuccess('Starting app in dev mode...'));
