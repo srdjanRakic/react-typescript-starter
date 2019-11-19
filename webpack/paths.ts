735743b1d26f54a6d00f89f57a245efed7dfa8b3
srdjanRakic <srgjan.rakic@gmail.com>
2019-11-19 09:04:20 +0100

    Add paths

---
+++ webpack/paths.ts
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const PROJECT_ROOT = path.resolve(__dirname, '../');
+const SOURCE_PATH = path.resolve(__dirname, '../src');
+
+module.exports = {
+  root: PROJECT_ROOT,
+  entryPath: path.join(SOURCE_PATH, 'index.ts'),
+  templatePath: path.resolve(__dirname, '../', 'public/index.html'),
+};
