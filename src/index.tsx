f3a23771dd94d8b96d1b5633d62fe688b2adb888
srdjanRakic <srgjan.rakic@gmail.com>
2019-11-26 09:50:03 +0100

    Add entry files

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>React TypeScript Starter </h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
